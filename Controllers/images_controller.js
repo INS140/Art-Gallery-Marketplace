ac13b56a2f249dd67a00bbf4843734ece8ced43e
MalMWare <malkelleymartin@gmail.com>
2023-04-16 13:25:47 -0700

    images_controller added

---
+++ Controllers/images_controller.js
@@ -0,0 +1,66 @@
+const images = require("express").Router()
+//const db = require('../models')
+
+//FIND ALL IMAGES
+images.get('/', (req, res) => {
+    try{
+
+    }
+    catch {
+        console.log(err)
+        res.status(500).json({ message: 'Server error'})
+    }
+})
+
+//FIND SPECIFIC IMAGES
+images.get('/:id', (req, res) => {
+    try {
+
+    }
+    catch {
+        console.log(err)
+        res.status(500).json({ message: 'Server error'})
+    }
+})
+
+//CREATE IMAGES
+images.post('/', (req, res) => {
+    try {
+        res.status(201).json({
+            message: 'Successfully insert a new images'
+        })
+    }
+    catch {
+        console.log(err)
+        res.status(500).json({ message: 'Server error' })
+    }
+})
+
+//UPDATE IMAGES INFORMATION
+images.put('/:id', (req, res) => {
+    try {
+        res.status(200).json({ 
+            message: 'Successfully updated images'
+        })
+    }
+    catch {
+        console.log(err)
+        res.status(500).json({ message: 'Server error'})
+    }
+})
+
+//DELETE AN IMAGES
+images.delete('/:id', (req, res) => {
+    try {
+        res.status(200).json({
+            message: `Successfully deleted image(s)`
+        })
+    }
+    catch {
+        console.log(err)
+        res.status(500).json({ message: 'Server error'})
+    }
+})
+
+//exports 
+module.exports = images
