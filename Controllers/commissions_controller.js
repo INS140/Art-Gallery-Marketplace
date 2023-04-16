d34e3d00851285a37941f9340596a8a59a649b24
MalMWare <malkelleymartin@gmail.com>
2023-04-16 13:33:39 -0700

    commissions_controller added

---
+++ Controllers/commissions_controller.js
@@ -0,0 +1,66 @@
+const commissions = require("express").Router()
+//const db = require('../models')
+
+//FIND ALL COMMISSIONS
+commissions.get('/', (req, res) => {
+    try{
+
+    }
+    catch {
+        console.log(err)
+        res.status(500).json({ message: 'Server error'})
+    }
+})
+
+//FIND SPECIFIC COMMISSIONS
+commissions.get('/:id', (req, res) => {
+    try {
+
+    }
+    catch {
+        console.log(err)
+        res.status(500).json({ message: 'Server error'})
+    }
+})
+
+//CREATE COMMISSIONS
+commissions.post('/', (req, res) => {
+    try {
+        res.status(201).json({
+            message: 'Successfully insert a new commissions'
+        })
+    }
+    catch {
+        console.log(err)
+        res.status(500).json({ message: 'Server error' })
+    }
+})
+
+//UPDATE COMMISSIONS INFORMATION
+commissions.put('/:id', (req, res) => {
+    try {
+        res.status(200).json({ 
+            message: 'Successfully updated commissions'
+        })
+    }
+    catch {
+        console.log(err)
+        res.status(500).json({ message: 'Server error'})
+    }
+})
+
+//DELETE AN COMMISSIONS
+commissions.delete('/:id', (req, res) => {
+    try {
+        res.status(200).json({
+            message: `Successfully deleted commission(s)`
+        })
+    }
+    catch {
+        console.log(err)
+        res.status(500).json({ message: 'Server error'})
+    }
+})
+
+//exports 
+module.exports = commissions
