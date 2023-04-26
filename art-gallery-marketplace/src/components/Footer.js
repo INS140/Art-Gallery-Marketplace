bd3185938309c78d04899a9903f01221eb385905
MalMWare <malkelleymartin@gmail.com>
2023-04-25 19:10:12 -0700

    added alignment to text

--- art-gallery-marketplace/src/components/Footer.js
+++ art-gallery-marketplace/src/components/Footer.js
@@ -4,8 +4,8 @@ import './css/footer.css'
 export default function Footer(){
   return <div className="bg-dark text-secondary">
     <hr></hr>
-    <h3>Contributors:</h3>
-    <ul className="list-inline">
+    <h3 className='text-center'>Contributors:</h3>
+    <ul className="list-inline text-center">
       <li className="list-unstyled list-inline-item"><a href='https://github.com/annielam0623' target='_blank'> Annie Lam </a></li>
       | &nbsp;
       <li className="list-unstyled list-inline-item"><a href='https://www.linkedin.com/in/isaac-saettel-110a76262/' target='_blank'> Isaac Saettel</a></li>
@@ -17,10 +17,12 @@ export default function Footer(){
       <li className="list-unstyled list-inline-item"><a href='https://www.linkedin.com/in/zmatero/' target='_blank'>Zane Matero</a></li>
     </ul>
     <p className='left'>Copyright © 2023 Art-Mart Platforms, Inc.</p>
+    <div className='text-center'>
     <img src='./images/FlagIcon.svg' alt='US' />
     <span>&nbsp; United States &nbsp; | &nbsp; English (US) &nbsp; | &nbsp; $ (USD)</span>
+    </div>
     <div>
-      <ul className="list-inline">
+      <ul className="list-inline text-center">
         <li className="list-unstyled list-inline-item">
           <a href="#">Terms of Use</a>
         </li>
