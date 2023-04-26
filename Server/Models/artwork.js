f8bf461d1d30f8029521764707748df4d689cd88
INS140 <IsaacNSaettel@gmail.com>
2023-04-25 21:16:38 -0700

    fixed server

--- Server/Models/artwork.js
+++ Server/Models/artwork.js
@@ -4,20 +4,6 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 // Schema
-<<<<<<< HEAD:art-gallery-marketplace/models/artwork.js
-const artworkSchema = new mongoose.Schema({
-  title: { type: String, required: true },
-  pic: { type: String, required: true },
-  artist: { type: String, required: true },
-  artist_id: { type: String, required: true },
-  description:{ type: String, required: true },
-  price: { type: String,   required: true },
-  year: { type: Date  },
-  style: { type: String },
-  size: { type: String,   required: true },
-  sold: { Boolean },
-  copies: { Number },
-=======
 const artworkSchema = new Schema({
   title: { type: String, required: true },
   pic: { type: String, required: true },
@@ -32,7 +18,6 @@ const artworkSchema = new Schema({
   size: { type: String, required: true },
   sold: Boolean,
   copies: Number,
->>>>>>> e1f0feba33d44408ebc145540d772d4d83d1b664:Server/Models/artwork.js
   })
 
 // Helper Methods
@@ -42,20 +27,4 @@ artworkSchema.methods.getArtworkBy = function () {
 
 // model
 const Artwork = mongoose.model('Artwork', artworkSchema)
-<<<<<<< HEAD:art-gallery-marketplace/models/artwork.js
-
-// find a specific artwork document by its ID and get its artist_id
-Artwork.findById('ARTWORK_ID')
-  .then((artwork) => {
-    console.log(artwork.artist_id);
-  })
-  .catch((err) => {
-    console.error(err);
-  });
-
 module.exports = Artwork;
-
-
-=======
-module.exports = Artwork;
->>>>>>> e1f0feba33d44408ebc145540d772d4d83d1b664:Server/Models/artwork.js
